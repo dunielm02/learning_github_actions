343401d525eb4ce54df6930a86862595e35b4d21
Duniel Mesa <dunielm02@gmail.com>
2024-05-17 00:10:51 -0400

    adding cache to image building

--- services/http_server/main.go
+++ services/http_server/main.go
@@ -5,6 +5,10 @@ import "net/http"
 func main() {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/papa", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Just for fun"))
+	})
+
 	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World from a container"))
 	})
